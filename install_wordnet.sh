e0cf91e4f3db2df7d5834e34c7fcea974b31bf32
John DeBovis <john.debovis@sparcedge.com>
2012-05-08 13:40:00 -0400

    add wornet install script

---
+++ install_wordnet.sh
@@ -0,0 +1,10 @@
+dir=`pwd`
+( cd $dir/temp/WordNet-3.0; make distclean)
+rm -rf $dir/temp
+mkdir $dir/temp
+curl http://wordnetcode.princeton.edu/3.0/WordNet-3.0.tar.gz > $dir/temp/wordnet.tar.gz
+tar -zxvf $dir/temp/wordnet.tar.gz 
+mv ./WordNet-3.0/ $dir/temp
+( cd $dir/temp/WordNet-3.0; ./configure)
+( cd $dir/temp/WordNet-3.0; make)
+( cd $dir/temp/WordNet-3.0; make install)
