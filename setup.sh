629ba0461883a3e3a46fb04754c5d772101e0346
John DeBovis <john.debovis@sparcedge.com>
2012-08-27 13:03:07 -0400

    remove old bson file, add new bson file, and updated the setup script for ubuntu

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-#sudo apt-get install wordnet tcl8.5 tk8.5 wordnet-base wordnet-gui
-sudo easy_install xlrd
+sudo apt-get install wordnet tcl8.5 tk8.5 wordnet-base wordnet-gui
+#sudo easy_install xlrd
 dir=`pwd`
 ( cd $dir/temp/WordNet-3.0; make distclean)
 rm -rf $dir/temp
@@ -7,8 +7,8 @@ mkdir $dir/temp
 curl http://wordnetcode.princeton.edu/3.0/WordNet-3.0.tar.gz > $dir/temp/wordnet.tar.gz
 tar -zxvf $dir/temp/wordnet.tar.gz 
 mv ./WordNet-3.0/ $dir/temp
-( cd $dir/temp/WordNet-3.0; ./configure --prefix=/usr/local)
-#( cd $dir/temp/WordNet-3.0; ./configure --prefix=/usr/local --with-tk=/usr/lib/tk8.5 --with-tcl=/usr/lib/tcl8.5)
+#( cd $dir/temp/WordNet-3.0; ./configure --prefix=/usr/local)
+( cd $dir/temp/WordNet-3.0; ./configure --prefix=/usr/local --with-tk=/usr/lib/tk8.5 --with-tcl=/usr/lib/tcl8.5)
 ( cd $dir/temp/WordNet-3.0; make)
 ( cd $dir/temp/WordNet-3.0; make install)
-mongorestore -d sparcin -c questions ./dump/sparcin/questions.bson
+#mongorestore -d sparcin -c questions ./dump/sparciq/similarity.bson
