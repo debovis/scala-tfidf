4f96cf1185b5d03e399f153d4c3c7e1a635d3ccb
John DeBovis <john.debovis@sparcedge.com>
2012-10-12 13:46:03 -0400

    add 2 string comparison demo

--- src/main/scala/com/sparcedge/analytics/service/AnalyticsService.scala
+++ src/main/scala/com/sparcedge/analytics/service/AnalyticsService.scala
@@ -15,6 +15,10 @@ import scala.io.Source
 import cc.spray.http.HttpMethods._
 
 import java.util.concurrent.TimeUnit.SECONDS
+//
+//import org.apache.commons.codec.binary.Base64
+//
+//import java.security.MessageDigest
 
 import com.sparcedge.analytics.indexers.matrix.TfIdfGenerator
 import com.sparcedge.analytics.similarity._
@@ -28,12 +32,22 @@ trait AnalyticsService extends Directives {
   	for(i <- 1 to 50) {
   		simActors = Actor.actorOf[SimilarityHandler].start() :: simActors
   	}
+  	
+//  	var nerActors = List[ActorRef]()
+//  	for(i <- 1 to 2) {
+//  		nerActors = Actor.actorOf[NERHandler].start() :: nerActors
+//  	}
+//  	
+//  	val nerLoadBalancer = Routing.loadBalancerActor(new CyclicIterator(nerActors))
+  	
 	val simLoadBalancer = Routing.loadBalancerActor(new CyclicIterator(simActors))
 	var configMap: Map[String,String]
 	
 	def similarityDatabase: SimilarityElementDatabase
 	def tfIdfManager: ActorRef
 	val demoHtml = Source.fromURL(getClass.getResource("/similarityDemo.html")).mkString
+	val compareHtml = Source.fromURL(getClass.getResource("/twoStringComparison.html")).mkString
+	
 	
 	val analyticsService = {
 		pathPrefix("static") {
@@ -41,25 +55,26 @@ trait AnalyticsService extends Directives {
 				getFromResourceDirectory("static")
 			}
 		} ~
-		(pathPrefix("similarity") & parameter("key")) { key =>
+		(pathPrefix("similarity") & parameter("key")) { key => ctx:RequestContext =>
+		  //println(ctx.request.headers)
 			get {
 				parameter("q") { query => ctx: RequestContext =>
 					simLoadBalancer ! similarityRequest(configMap,query, ctx, tfIdfManager)
 				}
 			} ~
-			path (IntNumber) { id =>
+			path (IntNumber) { id => 
 				put {
 					formFields("document")  { content => ctx: RequestContext =>
 					  // Spray not filtering correctly
 					  var httpMethod = ctx.request.method.toString()
 					  if(httpMethod == "PUT"){
-						tfIdfManager ! AddElement(TfIdfElement(id, "",content,null,false,true,false),key)
+					    tfIdfManager ! AddElement(TfIdfElement(id, "",content,null,false,true,false),key)
 						var responseText = "{\"created\": true}"
-						ctx.complete{
+						ctx.complete(
 						  HttpResponse(status = StatusCodes.OK, headers = Nil, content = HttpContent(`application/json`,responseText))
-						}
+						)
 					  }
-					}
+					}		    	
 				} ~
 				delete { ctx: RequestContext =>
 				  	// Spray not filtering correctly
@@ -73,6 +88,34 @@ trait AnalyticsService extends Directives {
 					}
 				}
 			}
+		}~
+//		path("ner") { 
+//		  get { 
+//			  parameter("q") { query => ctx:RequestContext =>
+//			      println(query)
+//			      nerLoadBalancer ! NerRequest(query,ctx,configMap)
+//			   }
+//		    
+//		  }
+//		}~
+		path("compare") {
+		  get {
+		    parameters("doc1","doc2") { (doc1,doc2) => ctx:RequestContext =>
+		      simLoadBalancer ! twoStringSimilarityRequest(doc1,doc2, ctx, tfIdfManager,configMap)
+		    }
+		  }
+		}~
+		path("compareDemo"){
+		  get { ctx:RequestContext =>
+		    var production = configMap.get("production-environment").get.toBoolean
+		    if(!production){
+		      completeCtx(ctx,compareHtml)
+		    }
+		    else {
+		      completeCtx(ctx,"Not available in production")
+		    }
+		    
+		  }
 		}~
 		path("similarityDemo") {
 			get { ctx: RequestContext =>
@@ -89,5 +132,15 @@ trait AnalyticsService extends Directives {
 	def completeCtx(ctx: RequestContext,text: String) = {
 	  ctx.complete(   HttpResponse(status = StatusCodes.OK, headers = Nil, content = HttpContent(`text/html`,text)))
 	}
+//	def md5(s:String) = {
+//	  MessageDigest.getInstance("md5").digest(s.getBytes())
+//	}
+				//					    ctx.request.headers.foreach{ el=>
+//					      	println("name: %s, value: %s".format(el.name,el.value))
+//					    	if(el.name == "Authorization"){
+//					    	  var authorizationResult = el.value.split(" ")(1)
+//					    	  var byteArray = Base64.decodeBase64(authorizationResult.getBytes())
+//					    	  println(new String((byteArray).map(_.toChar)))
+//		
 }
 
--- src/main/scala/com/sparcedge/analytics/similarity/SimilarityHandler.scala
+++ src/main/scala/com/sparcedge/analytics/similarity/SimilarityHandler.scala
@@ -8,10 +8,11 @@ import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 import java.util.LinkedHashMap
 import collection.JavaConversions._
+import scala.collection.SortedSet
 
 import org.slf4j.{Logger, LoggerFactory}
 
-import org.apache.commons.math3.linear.{OpenMapRealMatrix,RealMatrix,RealVector,ArrayRealVector}
+import org.apache.commons.math3.linear.{OpenMapRealMatrix,RealVector,ArrayRealVector}
 import org.apache.commons.collections15.Bag
 import org.apache.commons.collections15.bag.HashBag
 
@@ -26,6 +27,58 @@ class SimilarityHandler extends Actor {
 	val log = LoggerFactory.getLogger(getClass)
   
 	def receive = {
+	  
+	  	case twoStringSimilarityRequest (doc1,doc2,ctx,tfManager,configMap) =>
+	  	  
+	  	  	var wordSet: SortedSet[String] =  SortedSet[String]()
+	  	  	var documentWordFrequencyMap: Map[Int,HashBag[String]] = Map[Int,HashBag[String]]()
+	  	  	var documents = List[SimpleTFElement]()
+	  	  	documents = new SimpleTFElement(1,doc1,null) :: new SimpleTFElement(2,doc2,null) :: documents
+	  	  	
+	  	  	//  Get word Frequencies for documents
+	  	  	val doc1WordSet = WordFrequencyWrapper.getWordFrequencies(FrequencyType.WORDNET, doc1, configMap)
+	  	  	val doc2WordSet = WordFrequencyWrapper.getWordFrequencies(FrequencyType.WORDNET, doc2, configMap)
+	  	  	
+	  	  	// Get wordSet with all words and add words to Map
+	  	  	wordSet = wordSet ++ doc1WordSet.uniqueSet() ++ doc2WordSet.uniqueSet()
+	  	  	documentWordFrequencyMap += (1 -> doc1WordSet)
+	  	  	documentWordFrequencyMap += (2 -> doc2WordSet)
+	  	  	
+	  	  	// Initialize term frequency matrix
+	  	  	var numDocs = documents.size
+	  	  	var numWords = wordSet.size
+	  	  	// OpenMapRealMatrix(int rowDimension, int columnDimension) 
+	  	  	var tfMatrix = new OpenMapRealMatrix(numWords,numDocs)
+	  	  	var i,j=0
+	  	  	for(word <- wordSet){
+	  	  	  for(document <- documents){
+	  	  	    var wordFrequencies: HashBag[String] = documentWordFrequencyMap.get(document.id).get
+	  	  	    var count = wordFrequencies.getCount(word)
+	  	  	    tfMatrix.setEntry(i,j,count)
+	  	  	    j +=1
+	  	  	  }
+	  	  	  i+=1
+	  	  	  j=0
+	  	  	}
+	  	  	log.debug("created term frequency matrix with dimensions (" + tfMatrix.getRowDimension() + "," + tfMatrix.getColumnDimension() + ")");
+	  	  	
+	  	  	// Create vectors for comparison
+	  	  	var doc1Vect = tfMatrix.getColumnVector(0)
+	  	  	var doc2Vect = tfMatrix.getColumnVector(1)
+	  	  	
+	  	  	var similarityCosine:Double = 0.0
+	  	  	try{
+	  	  		similarityCosine = doc1Vect.cosine(doc2Vect)//Math.round(doc1Vect.cosine(doc2Vect)*100)/100;
+	  	  	}
+	  	  	catch{
+	  	  	  case e:Exception => 
+	  	  	    	log.error(e.printStackTrace.toString)
+	  	  	}
+	  	  	log.debug("returning %d documents with a similarity of %.2f".format(documents.size,similarityCosine))
+	  		ctx.complete (
+						response(StatusCodes.OK, compact(render("similarity" -> similarityCosine)))
+			)
+		
 		case similarityRequest	(configMap,value, ctx, tfManager) =>
 			val tfIdfGen = (tfManager ? TfIdfGeneratorRequest()).as[TfIdfGenerator].get
 			val minimumSimilarityQualifier = .3
@@ -96,5 +149,22 @@ class similarityResult (
 	val document : String
 )
 
-case class similarityRequest(configmap: Map[String,String],value: String, ctx: RequestContext, tfManager: ActorRef)
+case class twoStringSimilarityRequest(
+    val doc1: String,
+    val doc2: String,
+    val ctx: RequestContext,
+    val tfManager: ActorRef,
+    val configMap: Map[String,String]
+)
+
+case class similarityRequest(
+    configmap: Map[String,String],
+    value: String, 
+    ctx: RequestContext, 
+    tfManager: ActorRef
+ )
+ 
+ 
+ 
+ 
 
